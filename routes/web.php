efe0f3eb4d8c107070d72161eb6067e3c84fea53
natsuhiko.nishino <ec2-user@ip-172-31-4-211.us-east-2.compute.internal>
2018-06-08 07:38:59 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,5 @@ Route::get('/', 'tasksController@index');
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
